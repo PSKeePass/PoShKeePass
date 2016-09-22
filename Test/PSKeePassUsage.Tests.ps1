32fd31e3c4d8363fcc4c74ac598e25fdcee069fd
John Klann <jk7255@gmail.com>
2016-09-22 11:08:01 -0400

    Added Pester Tests
    New-KeePassPassword

--- Test/PSKeePassUsage.Tests.ps1
+++ Test/PSKeePassUsage.Tests.ps1
@@ -338,7 +338,7 @@ InModuleScope "PSKeePass" {
                 #     Get-KeePassDatabaseConfiguration | Should Be $null
                 # }
             #>
-            It "Example 1.4: Remove Database Configuration Profile - Invalid - No Profiles Exist." {
+            It "Example 1.2: Remove Database Configuration Profile - Invalid - No Profiles Exist." {
 
                 {Remove-KeePassDatabaseConfiguration -confirm:$false } | Should Throw "There are Currently No Database Configuration Profiles."
 
@@ -348,9 +348,144 @@ InModuleScope "PSKeePass" {
 
         New-KPConfigurationFile -Force
     }
+    
+    Describe "New-KeePassPassword - UnitTest" -Tag UnitTest {
 
+        Context "Example 1: Generate a new KeePass Password - Options" {
 
-    <#
+            It "Example 1.1: New Password using all basic options - Valid" {
+                New-KeePassPassword -UpperCase -LowerCase -Digits -SpecialCharacters -Minus -UnderScore -Space -Brackets -Length 20 | Should BeOfType System.Security.SecureString 
+            }
+
+            It "Example 1.2: New Password using all basic options + ExcludeLookALike - Valid" {
+                New-KeePassPassword -UpperCase -LowerCase -Digits -SpecialCharacters -Minus -UnderScore -Space -Brackets -Length 20 -ExcludeLookALike | Should BeOfType System.Security.SecureString 
+            }
+
+            It "Example 1.3: New Password using all basic options + NoRepeatingCharacters - Valid" {
+                New-KeePassPassword -UpperCase -LowerCase -Digits -SpecialCharacters -Minus -UnderScore -Space -Brackets -Length 20 -NoRepeatingCharacters | Should BeOfType System.Security.SecureString 
+            }
+
+            It "Example 1.4: New Password using some basic options + NoRepeatingCharacters - Invalid" {
+                { New-KeePassPassword -UpperCase -LowerCase -Digits -SpecialCharacters -Length 85 -NoRepeatingCharacters } | Should Throw 'Unabled to generate a password with the specified options.'
+            }
+
+            It "Example 1.5: New Password using all basic options + ExcludedCharactes - Valid" {
+                $SecurePass = New-KeePassPassword -UpperCase -LowerCase -Digits -SpecialCharacters -Minus -UnderScore -Space -Brackets -Length 70 -ExcludeCharacters '1,],-a'
+
+                $SecurePass |  Should BeOfType System.Security.SecureString
+                [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePass)) | Should Not Match ([regex]::Escape("^.*[1\]-a].*$")) 
+            }
+        }
+
+        Context "Example 2: Generate a new KeePass Password - Options - SaveAs" {
+
+            New-KPConfigurationFile -Force
+
+            It "Example 2.1: New Password using all basic options - Valid" {
+                New-KeePassPassword -UpperCase -LowerCase -Digits -SpecialCharacters -Minus -UnderScore -Space -Brackets -Length 20 -SaveAs 'Basic20' | Should BeOfType System.Security.SecureString
+
+                $PassProfile = Get-KPPasswordProfile -PasswordProfileName 'Basic20'
+                $PassProfile.Name | Should Be 'Basic20'
+                $PassProfile.CharacterSet | Should Be 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&''*+,./:;=?@\^`|~-_ []{}()<>'
+                $PassProfile.ExcludeLookAlike | Should Be 'False'
+                $PassProfile.NoRepeatingCharacters | Should Be 'False'
+                $PassProfile.ExcludeCharacters | Should Be ''
+                $PassProfile.Length | Should Be 20
+            }
+
+            It "Example 2.2: New Password using all basic options + ExcludeLookALike - Valid" {
+                New-KeePassPassword -UpperCase -LowerCase -Digits -SpecialCharacters -Minus -UnderScore -Space -Brackets -Length 20 -ExcludeLookALike -SaveAs 'BasicNoLookAlike20' | Should BeOfType System.Security.SecureString
+
+                $PassProfile = Get-KPPasswordProfile -PasswordProfileName 'BasicNoLookAlike20'
+                $PassProfile.Name | Should Be 'BasicNoLookAlike20'
+                $PassProfile.CharacterSet | Should Be 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&''*+,./:;=?@\^`|~-_ []{}()<>'
+                $PassProfile.ExcludeLookAlike | Should Be 'True'
+                $PassProfile.NoRepeatingCharacters | Should Be 'False'
+                $PassProfile.ExcludeCharacters | Should Be ''
+                $PassProfile.Length | Should Be 20 
+            }
+
+            It "Example 2.3: New Password using all basic options + NoRepeatingCharacters - Valid" {
+                New-KeePassPassword -UpperCase -LowerCase -Digits -SpecialCharacters -Minus -UnderScore -Space -Brackets -Length 20 -NoRepeatingCharacters -SaveAs 'BasicNoRepeat20' | Should BeOfType System.Security.SecureString
+                
+                $PassProfile = Get-KPPasswordProfile -PasswordProfileName 'BasicNoRepeat20'
+                $PassProfile.Name | Should Be 'BasicNoRepeat20'
+                $PassProfile.CharacterSet | Should Be 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&''*+,./:;=?@\^`|~-_ []{}()<>'
+                $PassProfile.ExcludeLookAlike | Should Be 'False'
+                $PassProfile.NoRepeatingCharacters | Should Be 'True'
+                $PassProfile.ExcludeCharacters | Should Be ''
+                $PassProfile.Length | Should Be 20 
+            }
+
+            It "Example 2.4: New Password using some basic options + NoRepeatingCharacters - Invalid" {
+                { New-KeePassPassword -UpperCase -LowerCase -Digits -SpecialCharacters -Length 85 -NoRepeatingCharacters -SaveAs 'BasicNoRepeatInvalid' } | Should Throw 'Unabled to generate a password with the specified options.'
+
+                Get-KPPasswordProfile -PasswordProfileName 'BasicNoRepeatInvalid' | Should Be $null
+            }
+
+            It "Example 2.5: New Password using all basic options + ExcludedCharactes - Valid" {
+                $SecurePass = New-KeePassPassword -UpperCase -LowerCase -Digits -SpecialCharacters -Minus -UnderScore -Space -Brackets -Length 70 -ExcludeCharacters '1,],-a' -SaveAs 'BasicExcudle1]-a'
+
+                $SecurePass |  Should BeOfType System.Security.SecureString
+                [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePass)) | Should Not Match ([regex]::Escape("^.*[1\]-a].*$"))
+
+                $PassProfile = Get-KPPasswordProfile -PasswordProfileName 'BasicExcudle1]-a'
+                $PassProfile.Name | Should Be 'BasicExcudle1]-a'
+                $PassProfile.CharacterSet | Should Be 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&''*+,./:;=?@\^`|~-_ []{}()<>'
+                $PassProfile.ExcludeLookAlike | Should Be 'False'
+                $PassProfile.NoRepeatingCharacters | Should Be 'False'
+                $PassProfile.ExcludeCharacters | Should Be '1,],-a'
+                $PassProfile.Length | Should Be 70
+            }
+        }
+
+        Context "Example 3: Generate a new KeePass Password - Profile" {
+
+            It "Example 3.1: New Password using Profile Basic20 - Valid" {
+                New-KeePassPassword  -PasswordProfileName 'Basic20' | Should BeOfType System.Security.SecureString
+            }
+
+            It "Example 3.2: New Password using Profile BasicNoLookAlike20 - Valid" {
+                New-KeePassPassword -PasswordProfileName 'BasicNoLookAlike20' | Should BeOfType System.Security.SecureString
+            }
+
+            It "Example 3.3: New Password using Profile BasicNoRepeat20 - Valid" {
+                New-KeePassPassword -PasswordProfileName 'BasicNoRepeat20' | Should BeOfType System.Security.SecureString
+            }
+
+            It "Example 3.4: New Password using Profile BasicNoRepeatInvalid - Invalid - Does Not Exist" {
+                { New-KeePassPassword -PasswordProfileName 'BasicNoRepeatInvalid' } | Should Throw
+            }
+
+            It "Example 3.5: New Password using Profile BasicExcudle1]-a - Valid" {
+                $SecurePass = New-KeePassPassword -PasswordProfileName 'BasicExcudle1]-a'
+
+                $SecurePass |  Should BeOfType System.Security.SecureString
+                [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePass)) | Should Not Match ([regex]::Escape("^.*[1\]-a].*$"))
+            }
+        }
+    }
+
+<#
+    Describe "New-KeePassEntry - UnitTest" -Tag UnitTest {
+        New-KPConfigurationFile -Force
+
+        Context "Example 1: Creates a New KeePass Entry." {
+
+            It "Example 1.1: Remove Database Configuration Profile - Valid - By Name" {
+                New-KeePassDatabaseConfiguration -DatabaseProfileName 'SampleProfile' -DatabasePath "$($PSScriptRoot)\Includes\AuthenticationDatabases\MasterKey.kdbx" -UseNetworkAccount | Should Be $null
+
+                # Get-KeePassDatabaseConfiguration -DatabaseProfileName 'SampleProfile'
+
+                Remove-KeePassDatabaseConfiguration -DatabaseProfileName 'SampleProfile' -confirm:$false | Should Be $null
+
+                Get-KeePassDatabaseConfiguration -DatabaseProfileName 'SampleProfile' | Should Be $null
+            }
+        }
+
+        New-KPConfigurationFile -Force
+    }
+
     Describe "Get-KPGroup - UnitTest" -Tag UnitTest {
         $KeePassCredential = Get-KPCredential -DatabaseFile "$PSScriptRoot\Includes\PSKeePassTestDatabase.kdbx" -KeyFile "$PSScriptRoot\Includes\PSKeePassTestDatabase.key"
         $KeePassConnection = Get-KPConnection -KeePassCredential $KeePassCredential
