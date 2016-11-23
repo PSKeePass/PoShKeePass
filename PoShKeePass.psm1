d25fef02933262110f90622d54ca7fb4c3fe68dc
John Klann <dba@jkdba.com>
2016-11-23 12:04:35 -0500

    Updates to New-KPConnection
    1. Added Default ParameterSet
    2. Removed Credential Parameter.
    3. Changed Masterkey Parameter to Object Type. Allows Nulls, Performs
    type validation in function and checks for Type SecureString and
    PSCredential.
    4. Reordered and added Positions to parameters in a more logical way.
    5. Added Inline Comments.
    6. Add More specific logic in regards to parameterset being used to make
    the code more strucutred and easier to update in the future.
    7.  Split UseMasterKey Logic between parameter sets for less complex
    easier to manage code.
    8.  User will only get prompted for the master key (via Credential
    prompt) if they specify a Profile that requires one without specifying
    the MasterKey Parameter.
    9. Updated format to be consistent with rest of module

--- PoShKeePass.psm1
+++ PoShKeePass.psm1
@@ -1889,91 +1889,132 @@ function New-KPConnection
         .PARAMETER UseWindowsAccount
             Use the current windows account as an authentication method
     #>
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory=$true, ParameterSetName='CompositeKey')]
-        [ValidateNotNullOrEmpty()]
-        [String] $Database,
-
-        [Parameter(Mandatory=$true, ParameterSetName='Profile')]
+    [CmdletBinding(DefaultParameterSetName='Profile')]
+    param
+    (
+        [Parameter(Position=0, Mandatory=$true, ParameterSetName='Profile')]
         [ValidateNotNullOrEmpty()]
         [String] $DatabaseProfileName,
 
-        [Parameter(Mandatory=$false, ParameterSetName='CompositeKey')]
-        [Parameter(Mandatory=$false, ParameterSetName='Profile')]
-        [AllowNull()]
-        [System.Security.SecureString] $MasterKey,
-
-        [Parameter(Mandatory=$false, ParameterSetName='CompositeKey')]
-        [Parameter(Mandatory=$false, ParameterSetName='Profile')]
+        [Parameter(Position=0, Mandatory=$true, ParameterSetName='CompositeKey')]
         [ValidateNotNullOrEmpty()]
-        [System.Management.Automation.PSCredential] $Credential,
+        [String] $Database,
 
-        [Parameter(Mandatory=$false, ParameterSetName='CompositeKey')]
+        
+        [Parameter(Position=2, Mandatory=$false, ParameterSetName='CompositeKey')]
+        [Parameter(Position=1, Mandatory=$false, ParameterSetName='Profile')]
+        [AllowNull()]
+        [PSObject] $MasterKey,
+
+        [Parameter(Position=1, Mandatory=$false, ParameterSetName='CompositeKey')]
         [ValidateNotNullOrEmpty()]
         [String] $KeyPath,
 
-        [Parameter(ParameterSetName='CompositeKey')]
-        [switch] $UseWindowsAccount
+        [Parameter(Position=3, ParameterSetName='CompositeKey')]
+        [Switch] $UseWindowsAccount
     )
     process
     {
-        try {
+        ## Create KP Database Object
+        try
+        {
             $DatabaseObject = New-Object -TypeName KeepassLib.PWDatabase -ErrorAction Stop
-        } catch {
+        }
+        catch
+        {
             Import-KPLibrary
             $DatabaseObject = New-Object -TypeName KeepassLib.PWDatabase -ErrorAction Stop
         }
 
+        ## Create KP CompositeKey Object
         $CompositeKey = New-Object -TypeName KeepassLib.Keys.CompositeKey
 
-       if ($PSCmdlet.ParameterSetName -eq 'Profile') {
-           $KeepassConfigurationObject = Get-KeePassDatabaseConfiguration -DatabaseProfileName $DatabaseProfileName
-           if (-not $KeepassConfigurationObject) {
-               throw 'InvalidKeePassConfiguration : No KeePass Configuration has been created.'
-           }
-           $Database = $KeepassConfigurationObject.DatabasePath
-           $KeyPath = $KeepassConfigurationObject.KeyPath
-           $UseWindowsAccount = $KeepassConfigurationObject.UseWindowsAccount
-       }
-
-        $UseMasterKey = $KeepassConfigurationObject.UseMasterKey -or (($PSCmdlet.ParameterSetName -eq "CompositeKey") -and ($MasterKey -or $Credential.Password))
-
-        $DatabaseItem = Get-Item -Path $Database -ErrorAction Stop
-        if (($Credential.Password) -and $UseMasterKey) {
-            $CompositeKey.AddUserKey((New-Object KeepassLib.Keys.KcpPassword([System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Credential.Password)))))
+        ## Validate MasterKey Type
+        if(($MasterKey -isnot [PSCredential]) -and ($MasterKey -isnot [SecureString]) -and $MasterKey)
+        {
+            Write-Error -Message ('[PROCESS] The MasterKey of type: ({0}). Is not Supported Please supply a MasterKey of Types (SecureString or PSCredential).' -f $($MasterKey.GetType().Name)) -Category InvalidType -TargetObject $MasterKey -RecommendedAction 'Provide a MasterKey of Type PSCredential or SecureString'
         }
-        if (($MasterKey) -and ($UseMasterKey)) {
+
+        ## Get Profile Values
+        if($PSCmdlet.ParameterSetName -eq 'Profile')
+        {
+            $KeepassConfigurationObject = Get-KeePassDatabaseConfiguration -DatabaseProfileName $DatabaseProfileName
+
+            if(-not $KeepassConfigurationObject) 
+            {
+                throw 'InvalidKeePassConfiguration : No KeePass Configuration has been created.'
+            }
+
+            $Database = $KeepassConfigurationObject.DatabasePath
+            $KeyPath = $KeepassConfigurationObject.KeyPath
+            $UseWindowsAccount = $KeepassConfigurationObject.UseWindowsAccount
+            $UseMasterKey = $KeepassConfigurationObject.UseMasterKey
+            
+            ## Prompt for MasterKey if specified in the profile and was not provided.
+            if($UseMasterKey -and -not $MasterKey)
+            {
+                $Host.ui.PromptForCredential('KeePassCredential', 'Please enter your KeePass password.', 'KeePass', 'KeePass')
+            }
+        }
+        ## Added this separation for easier future Management.
+        elseif($PSCmdlet.ParameterSetName -eq 'CompositeKey')
+        {
+            $UseMasterKey = if($MasterKey){ $true }
+        }
+
+        ## Handle if the master key is a PSCredential
+        if($MasterKey -is [PSCredential])
+        {
+            [SecureString] $MasterKey = $MasterKey.Password
+        }
+
+        ##Exception : I will want to handle this error in a more user friendly error, as is the style of the rest of this module.
+        $DatabaseItem = Get-Item -Path $Database -ErrorAction Stop
+
+        ## Start Building CompositeKey
+        ## Order in which the CompositeKey is created is important and must follow the order of : MasterKey, KeyFile, Windows Account
+        if($UseMasterKey)
+        {
             $CompositeKey.AddUserKey((New-Object KeepassLib.Keys.KcpPassword([System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($MasterKey)))))
         }
-        if ($KeyPath) {
-            try {
+
+        if($KeyPath)
+        {
+            try
+            {
+                ##Exception : I will want to handle this error in a more user friendly error, as is the style of the rest of this module.
                 $KeyPathItem = Get-Item $KeyPath -ErrorAction Stop
                 $CompositeKey.AddUserKey((New-Object KeepassLib.Keys.KcpKeyfile($KeyPathItem.FullName)))
-            } catch {
+            }
+            catch
+            {
+                ##Exception : I will want to handle this error in a more user friendly error, as is the style of the rest of this module.
                 Write-Warning ('Could not read the specfied Key file [{0}].' -f $KeyPathItem.FullName)
             }
         }
-        if ($UseWindowsAccount) {
+
+        if($UseWindowsAccount)
+        {
             $CompositeKey.AddUserKey((New-Object KeepassLib.Keys.KcpUserAccount))
         }
 
-        if ($CompositeKey.UserKeyCount -le 0) {
-            $Credential = $Host.ui.PromptForCredential('KeePassCredential', 'Please enter your KeepPass password.', 'KeePass', 'KeePass')
-            $CompositeKey.AddUserKey((New-Object KeepassLib.Keys.KcpPassword([System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Credential.Password)))))
-        }
-
+        ## Create IOConnection Object
         $IOInfo = New-Object KeepassLib.Serialization.IOConnectionInfo
         $IOInfo.Path = $DatabaseItem.FullName
+
+        ## We currently are not using a status logger hence the null.
         $IStatusLogger = New-Object KeePassLib.Interfaces.NullStatusLogger
 
+        ## Connect, Open and Return Database Object
         $DatabaseObject.Open($IOInfo, $CompositeKey, $IStatusLogger) | Out-Null
-
         $DatabaseObject
-        if (-not $DatabaseObject.IsOpen) {
+
+        ##Exception : I will want to handle this error in a more user friendly error, as is the style of the rest of this module.
+        if(-not $DatabaseObject.IsOpen)
+        {
             Throw 'InvalidDatabaseConnectionException : The database is not open.'
         }
-    }   
+    }
 }
 
 function Remove-KPConnection
