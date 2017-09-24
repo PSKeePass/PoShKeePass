5cdd656faa80dcf14830ad499f9cd49ed9d5c533
Skons <k.temming@gmail.com>
2017-09-24 10:21:12 +0200

    added New-KeePassDatabase to dev, with indentation fix

--- PoShKeePass.psm1
+++ PoShKeePass.psm1
@@ -1579,6 +1579,92 @@ function Remove-KeePassDatabaseConfiguration
     }
 }
 
+function New-KeePassDatabase 
+{
+    <#
+        .SYNOPSIS
+            Function to create a keepass database.
+        .DESCRIPTION
+            This function creates a new keepass database
+        .PARAMETER DatabasePath
+            Path to the Keepass database (.kdbx file)
+        .PARAMETER KeyPath
+            Not yet implemented
+        .PARAMETER UseNetworkAccount
+            Specify of you want the database to use windows authentication
+        .PARAMETER MasterKey
+            The masterkey that provides access to the database
+        .INPUTS
+            String
+            SecureString
+        .OUTPUTS
+            $null
+    #>
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Position=0, Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [String] $DatabasePath,
+
+        [Parameter(Position=1, Mandatory=$true, ValueFromPipeline=$false, ParameterSetName='Key')]
+        [Parameter(Position=1, Mandatory=$true, ValueFromPipeline=$false, ParameterSetName='KeyAndMaster')]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript({Test-Path $_})]
+        [String] $KeyPath,
+
+        [Parameter(Position=2, Mandatory=$false, ValueFromPipeline=$false, ParameterSetName='Key')]
+        [Parameter(Position=2, Mandatory=$false, ValueFromPipeline=$false, ParameterSetName='Master')]
+        [Parameter(Position=2, Mandatory=$true, ValueFromPipeline=$false, ParameterSetName='Network')]
+        [Switch] $UseNetworkAccount,
+
+        [Parameter(Position=3, Mandatory=$true, ValueFromPipeline=$false, ParameterSetName='Master')]
+        [Parameter(Position=3, Mandatory=$true, ValueFromPipeline=$false, ParameterSetName='KeyAndMaster')]
+        [PSCredential] $MasterKey
+    )
+
+    begin {
+        if ($KeyPath) {
+            thow "KeyPath is not implemented yet"
+        }
+    }
+    process
+    {
+        try
+        {
+            $DatabaseObject = New-Object -TypeName KeepassLib.PWDatabase -ErrorAction Stop
+        }
+        catch
+        {
+            Import-KPLibrary
+            $DatabaseObject = New-Object -TypeName KeepassLib.PWDatabase -ErrorAction Stop
+        }
+
+        ## Create KP CompositeKey Object
+        $CompositeKey = New-Object -TypeName KeepassLib.Keys.CompositeKey
+
+        if($MasterKey)
+        {
+            $KcpPassword = New-Object -TypeName KeePassLib.Keys.KcpPassword($MasterKey.GetNetworkCredential().Password)
+            $CompositeKey.AddUserKey($KcpPassword)
+        }
+
+        #if masterkey is specified, it should
+        if($UseNetworkAccount)
+        {
+            $CompositeKey.AddUserKey((New-Object KeepassLib.Keys.KcpUserAccount))
+        }
+
+        $IOInfo = New-Object KeepassLib.Serialization.IOConnectionInfo
+        $IOInfo.Path = $DatabasePath
+
+        $IStatusLogger = New-Object KeePassLib.Interfaces.NullStatusLogger
+
+        $DatabaseObject.New($IOInfo, $CompositeKey) | Out-Null
+        $DatabaseObject.Save($IStatusLogger)
+    }
+}
+
 <#
 # Internals
 # *These functions below support all of the functions above.
