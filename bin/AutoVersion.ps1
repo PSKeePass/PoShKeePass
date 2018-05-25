72d49477b400f00d77e1630b69afe18ea6367e59
John Klann <dba@jkdba.com>
2018-05-25 09:51:35 -0400

    Fixed regex for auto versioning
    need since format of psd1 field changed due to formatting.

--- bin/AutoVersion.ps1
+++ bin/AutoVersion.ps1
@@ -1,20 +1,21 @@
 ## Get psd1 file
 $RawPSD = Get-Content -Path "$($PSScriptRoot)\..\PoShKeePass.psd1"
-$ModuleVersion=($RawPSD | ? { $_ -match "^ModuleVersion.*$" }) -replace "^.*=\s|'"
+$ModuleVersion = ($RawPSD | ? { $_ -match "^\s+ModuleVersion.+$" }) -replace '.+(\d\.\d\.\d\.\d).$', '$1'
 
 [Int[]] $VerArr = $ModuleVersion -split '\.'
 
 $NewModuleVersion = ''
 $CarryValue = 1
-$EntryPoint = $($VerArr.Count -1)
-for($i=$EntryPoint; $i -ge 0; $i--){
+$EntryPoint = $($VerArr.Count - 1)
+for($i = $EntryPoint; $i -ge 0; $i--)
+{
     $VersionPart = $VerArr[$i]
 
     $VersionPart += $CarryValue
 
     if($i -eq 0)
     {
-        $NewModuleVersion = "$VersionPart"+$NewModuleVersion
+        $NewModuleVersion = "$VersionPart" + $NewModuleVersion
         break
     }
     else
@@ -27,10 +28,12 @@ for($i=$EntryPoint; $i -ge 0; $i--){
         {
             $CarryValue = 0
         }
-        $NewModuleVersion = ".$VersionPart"+$NewModuleVersion
+        $NewModuleVersion = ".$VersionPart" + $NewModuleVersion
     }
 }
 
 Write-Verbose -Message "New Version: $NewModuleVersion"
 
-$RawPSD | % { $_ -replace "(?<=^ModuleVersion = ')\d+\.\d\.\d\.\d(?=')",$NewModuleVersion } | Out-File -FilePath  "$($PSScriptRoot)\..\PoShKeePass.psd1" -Force
+$RawPSD | % { $_ -replace "(?<=^ModuleVersion = ')\d+\.\d\.\d\.\d(?=')", $NewModuleVersion } | Out-File -FilePath  "$($PSScriptRoot)\..\PoShKeePass.psd1" -Force
+
+##     ModuleVersion     = '2.0.4.5'
