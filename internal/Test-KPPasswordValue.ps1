976e161d9d9fe7f11f022f66db67a2e3cfa48948
John Klann <dba@jkdba.com>
2018-05-25 11:57:47 -0400

    Consolidated Error Raising into function
    - updated to adv func
    - added parameter and validation attributes

--- internal/Test-KPPasswordValue.ps1
+++ internal/Test-KPPasswordValue.ps1
@@ -1,23 +1,30 @@
 function Test-KPPasswordValue
 {
+    [cmdletbinding()]
     param
     (
-        [PSObject] $PassValue
+        [Parameter(Position = 0)]
+        [AllowNull()] [AllowEmptyString()]
+        [PSObject] $KeePassPassword
     )
-    if(-not $PassValue)
+
+    if(-not $KeePassPassword)
     {
         $true
     }
-    elseif($PassValue.GetType().Name -eq 'SecureString')
+    elseif($KeePassPassword.GetType().Name -eq 'SecureString')
     {
         $true
     }
-    elseif($PassValue.GetType().Name -eq 'ProtectedString')
+    elseif($KeePassPassword.GetType().Name -eq 'ProtectedString')
     {
         $true
     }
     else
     {
         $false
+        Write-Warning -Message '[PROCESS] Please provide a KeePassPassword Of Type SecureString or KeePassLib.Security.ProtectedString.'
+        Write-Warning -Message ('[PROCESS] The Value supplied ({0}) is of Type {1}.' -f $KeePassPassword, $KeePassPassword.GetType().Name)
+        Write-Error -Message 'Please provide a KeePassPassword Of Type SecureString or KeePassLib.Security.ProtectedString.' -ea Stop
     }
 }
