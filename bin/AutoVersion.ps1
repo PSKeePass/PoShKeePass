a199b2ed7ea2fc7d4debfe9c77deb85ed3f4b67b
John Klann <dba@jkdba.com>
2018-05-25 10:08:10 -0400

    Fixed Auto Versioning script

--- bin/AutoVersion.ps1
+++ bin/AutoVersion.ps1
@@ -1,12 +1,17 @@
+[Cmdletbinding()]
+param()
+
 ## Get psd1 file
 $RawPSD = Get-Content -Path "$($PSScriptRoot)\..\PoShKeePass.psd1"
 $ModuleVersion = ($RawPSD | ? { $_ -match "^\s+ModuleVersion.+$" }) -replace '.+(\d\.\d\.\d\.\d).$', '$1'
 
+Write-Verbose -Message ('ModuleVersion Found: ' + $ModuleVersion)
 [Int[]] $VerArr = $ModuleVersion -split '\.'
 
 $NewModuleVersion = ''
 $CarryValue = 1
 $EntryPoint = $($VerArr.Count - 1)
+
 for($i = $EntryPoint; $i -ge 0; $i--)
 {
     $VersionPart = $VerArr[$i]
@@ -34,6 +39,4 @@ for($i = $EntryPoint; $i -ge 0; $i--)
 
 Write-Verbose -Message "New Version: $NewModuleVersion"
 
-$RawPSD | % { $_ -replace "(?<=^ModuleVersion = ')\d+\.\d\.\d\.\d(?=')", $NewModuleVersion } | Out-File -FilePath  "$($PSScriptRoot)\..\PoShKeePass.psd1" -Force
-
-##     ModuleVersion     = '2.0.4.5'
+$RawPSD | ForEach-Object { $_ -replace $ModuleVersion, $NewModuleVersion } | Out-File -FilePath  "$($PSScriptRoot)\..\PoShKeePass.psd1" -Force
