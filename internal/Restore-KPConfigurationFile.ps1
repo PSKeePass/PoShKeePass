77e47f56e4ffa7f32950656b636c9144a1ec214f
John Klann <dba@jkdba.com>
2018-06-25 13:00:36 -0400

    Formatting

--- internal/Restore-KPConfigurationFile.ps1
+++ internal/Restore-KPConfigurationFile.ps1
@@ -14,20 +14,25 @@ function Restore-KPConfigurationFile
     [CmdletBinding()]
     param
     (
-
+        [Parameter(Position = 0)]
+        [String] $BreakingChangesVersion
     )
     process
     {
         $ReturnStatus = $false
         $Path = Resolve-Path -Path ('{0}\..' -f $PSScriptRoot)
+
         Write-Verbose -Message ('[PROCESS] Checking if there is a previous KeePassConfiguration.xml file to be loaded from: {0}.' -f $Path.Path )
-        $PreviousVerision = ((Get-ChildItem $Path.Path).Name | Sort-Object -Descending | Select-Object -First 2)[1]
+        $PreviousVersion = ((Get-ChildItem $Path.Path).Name | Sort-Object -Descending | Select-Object -First 2)[1]
+
         Write-Verbose -Message ('PreviousVersion: {0}.' -f $PreviousVersion)
-        $PreviousVerisionConfigurationFile = Resolve-Path -Path ('{0}\..\{1}\KeePassConfiguration.xml' -f $PSScriptRoot, $PreviousVerision) -ErrorAction SilentlyContinue -ErrorVariable GetPreviousConfigurationFileError
-        if(-not $GetPreviousConfigurationFileError -and $PreviousVerision)
+        $PreviousVersionConfigurationFile = Resolve-Path -Path ('{0}\..\{1}\KeePassConfiguration.xml' -f $PSScriptRoot, $PreviousVersion) -ErrorAction SilentlyContinue -ErrorVariable GetPreviousConfigurationFileError
+
+        if(-not $GetPreviousConfigurationFileError -and $PreviousVersion)
         {
-            Write-Verbose -Message ('[PROCESS] Copying last Configuration file from the previous version ({0}).' -f $PreviousVerision)
-            Copy-Item -Path $PreviousVerisionConfigurationFile -Destination "$PSScriptRoot" -ErrorAction SilentlyContinue -ErrorVariable RestorePreviousConfigurationFileError
+            Write-Verbose -Message ('[PROCESS] Copying last Configuration file from the previous version ({0}).' -f $PreviousVersion)
+            Copy-Item -Path $PreviousVersionConfigurationFile -Destination "$PSScriptRoot" -ErrorAction SilentlyContinue -ErrorVariable RestorePreviousConfigurationFileError
+
             if($RestorePreviousConfigurationFileError)
             {
                 Write-Warning -Message '[PROCESS] Unable to restore previous KeePassConfiguration.xml file. You will need to copy your previous file from your previous module version folder or create a new one.'
